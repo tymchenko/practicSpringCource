bcb6734d83531119ecde59beb3794369b2521a10
Ivan Tymchenko <tycmhenkolviv@gmail.com>
2018-08-11 23:05:07 +0300

    created spring.xml, User, UserService classes

---
+++ spring_core_study/src/main/java/services/UserService.java
@@ -0,0 +1,28 @@
+package services;
+
+import vo.User;
+
+import java.util.List;
+
+public class UserService {
+
+    private void save(User user){
+
+    }
+
+    private void remove (User user){
+
+    }
+
+    private User getById(long id){
+        return null;
+    }
+
+    private User getByEmail(String email){
+        return null;
+    }
+
+    private List<User> getAllUsers(){
+        return null;
+    }
+}
