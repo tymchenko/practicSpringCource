046838aa1a8e79eddb0ff62a901caf42000034d7
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-13 12:34:02 +0300

    created AuditoriumEvent relation

---
+++ spring_core_study/src/main/java/db/AuditoriumEvent.java
@@ -0,0 +1,12 @@
+package db;
+
+import vo.Auditorium;
+import vo.Event;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AuditoriumEvent {
+    Map<Auditorium, List<Event>> auditoriumEventRelation = new HashMap<>();
+}
