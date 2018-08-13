144741bef7e16ee90be4fb267468e4c972a9f188
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-13 12:24:20 +0300

    created List of discount strategies

--- spring_core_study/src/main/java/services/discount/DiscountService.java
+++ spring_core_study/src/main/java/services/discount/DiscountService.java
@@ -4,12 +4,12 @@ import vo.Event;
 import vo.User;
 
 import java.util.Date;
+import java.util.List;
 
 public class DiscountService {
+    private List<DiscountStrategy> strategies;
 
     public int getDiscount(User user, Event event, Date dateTime, int numberOfTickets){
         return 0;
     }
-
-
 }
