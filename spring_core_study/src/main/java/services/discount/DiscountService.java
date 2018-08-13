f4b5512e68723f59ae91d31590d5cbc1f038df13
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-13 12:22:51 +0300

    put DiscountService to discount package and created getDiscount method

---
+++ spring_core_study/src/main/java/services/discount/DiscountService.java
@@ -0,0 +1,15 @@
+package services.discount;
+
+import vo.Event;
+import vo.User;
+
+import java.util.Date;
+
+public class DiscountService {
+
+    public int getDiscount(User user, Event event, Date dateTime, int numberOfTickets){
+        return 0;
+    }
+
+
+}
