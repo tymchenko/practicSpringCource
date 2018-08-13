e386ff9c94d836372e4586a466c77266d4806d1c
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-13 12:29:53 +0300

    created TicketDao and Map<Event, List<Ticket>> in it

--- spring_core_study/src/main/java/db/TicketDao.java
+++ spring_core_study/src/main/java/db/TicketDao.java
@@ -1,4 +1,12 @@
 package db;
 
+import vo.Event;
+import vo.Ticket;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TicketDao {
+    private Map<Event, List<Ticket>> tickets = new HashMap<>();
 }
