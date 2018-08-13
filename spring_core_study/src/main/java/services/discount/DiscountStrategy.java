f7af1eae799a01e7c1cb0c1e7734bc66c47ca6c1
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-13 12:21:50 +0300

    created DisountStrategy

---
+++ spring_core_study/src/main/java/services/discount/DiscountStrategy.java
@@ -0,0 +1,8 @@
+package services.discount;
+
+
+/*Birthday strategy - give 5% if user has birthday within 5 days of air date
+   Every 10th ticket - give 50% for every 10th ticket purchased by user. This strategy can also be applied for not-registered users if 10 or more tickets are bought
+ */
+public class DiscountStrategy {
+}
