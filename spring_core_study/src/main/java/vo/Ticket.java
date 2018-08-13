fc77ee5a811a826b5dca00ac24b29f2ac0bc86df
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-13 12:25:55 +0300

    added User and list of events to Ticket

--- spring_core_study/src/main/java/vo/Ticket.java
+++ spring_core_study/src/main/java/vo/Ticket.java
@@ -1,4 +1,24 @@
 package vo;
 
+import java.util.List;
+
 public class Ticket {
+    private User user;
+    private List<Event> events;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
 }
