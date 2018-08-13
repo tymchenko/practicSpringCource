b2f74f0a5b6006d5b31555a244cd4e21703f39e7
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-13 19:21:45 +0300

    created BookingServiceTest

---
+++ spring_core_study/src/test/java/services/BookingServiceTest.java
@@ -0,0 +1,40 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceTest {
+
+    @Test
+    void getTickets() {
+    }
+
+    @Test
+    void calculateDiscount() {
+    }
+
+    @Test
+    void getCost() {
+    }
+
+    @Test
+    void getVipCost() {
+    }
+
+    @Test
+    void getAllPrices() {
+    }
+
+    @Test
+    void bookTickets() {
+    }
+
+    @Test
+    void bookTicket() {
+    }
+
+    @Test
+    void getBookedTickets() {
+    }
+}
