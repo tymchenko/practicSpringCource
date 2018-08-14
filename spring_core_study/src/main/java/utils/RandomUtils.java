4b727fa3f1c13b3ba9f516940095b7c934f002c8
Ivan Tymchenko <tymchenkolviv@gmail.com>
2018-08-14 17:58:15 +0300

    refactored RandomUtils: removed bean, removed unused comments

--- spring_core_study/src/main/java/utils/RandomUtils.java
+++ spring_core_study/src/main/java/utils/RandomUtils.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 class RandomUtils {
     private Random random = new Random();
-//    private RandomUtils(){}
 
     public Long getRandomLong() {
         return random.nextLong();
